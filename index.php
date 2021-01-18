1c05f0d144401d4921f53abde37276e36274acc2
ValentinaNovarino <valenovarino@gmail.com>
2021-01-18 15:16:31 +0100

    definito classe

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
- 
+// definire la struttura di una tabella users che contiene i dati degli utenti di un blog
+// definire una classe User che "mappa" la tabella users
+class User {
+    public $name;
+    public $lastname;
+    public $userName;
+    public $password;
+    public $email;
+    public $age;
+}
+
 ?>
