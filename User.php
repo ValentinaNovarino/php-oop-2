8d7edda27c28eae509ccd19a3f32911e700766e9
ValentinaNovarino <valenovarino@gmail.com>
2021-01-18 15:26:54 +0100

    diviso index.php e User.php

---
+++ User.php
@@ -0,0 +1,11 @@
+<?php
+// definire una classe User che "mappa" la tabella users
+class User {
+    public $name;
+    public $lastname;
+    public $userName;
+    public $password;
+    public $email;
+    public $age;
+}
+?>
--- index.php
+++ index.php
@@ -1,17 +1,13 @@
 <?php
-// definire la struttura di una tabella users che contiene i dati degli utenti di un blog
-// definire una classe User che "mappa" la tabella users
-class User {
-    public $name;
-    public $lastname;
-    public $userName;
-    public $password;
-    public $email;
-    public $age;
-}
+// includo User.php nel file index.php
+require('User.php');
 
 // creare alcune istanze della classe User per stampare i dati degli utenti
 $user1 = new User;
 $user2 = new User;
 $user3 = new User;
+
+var_dump($user1);
+var_dump($user2);
+var_dump($user3);
 ?>
