2a4e78e4db2d47005f052da8ce9f0309943e816d
ValentinaNovarino <valenovarino@gmail.com>
2021-01-18 16:16:04 +0100

    creato constructor

--- User.php
+++ User.php
@@ -1,11 +1,20 @@
 <?php
 // definire una classe User che "mappa" la tabella users
 class User {
+    public $user_id;
     public $name;
     public $lastname;
     public $userName;
     public $password;
     public $email;
     public $age;
+    public $telephone;
+    public $address;
+
+    function __construct($_userName, $_password = '********', $_email = '*****@gmail.com') {
+        $this->userName = $_userName;
+        $this->password = $_password;
+        $this->email = $_email;
+    }
 }
 ?>
--- index.php
+++ index.php
@@ -2,15 +2,29 @@
 // includo User.php nel file index.php
 require('User.php');
 
-// creare alcune istanze della classe User
-$user1 = new User;
-$user2 = new User;
-$user3 = new User;
+// creare alcune istanze della classe User per stampare i dati degli utenti
+$user1 = new User('LRossi32');
+$user2 = new User('SVerdi22');
+$user3 = new User('GNeri45');
 
+$user1->name = 'Luca';
+$user1->lastname = 'Rossi';
+$user1->age = 32;
+$user1->telephone = '123456';
+$user1->address = 'Via dei mille 20 Torino';
 var_dump($user1);
+
+$user2->name = 'Silvia';
+$user2->lastname = 'Verdi';
+$user2->age = 22;
+$user2->telephone = '123456';
+$user2->address = 'Via dei mille 20 Torino';
 var_dump($user2);
+
+$user3->name = 'Giulio';
+$user3->lastname = 'Neri';
+$user3->age = 45;
+$user3->telephone = '123456';
+$user3->address = 'Via dei mille 20 Torino';
 var_dump($user3);
-
-// stampare i dati degli utenti
-
 ?>
