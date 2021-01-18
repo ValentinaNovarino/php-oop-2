cd89324176ad7711c001a34d81cf8e2ead611cde
ValentinaNovarino <valenovarino@gmail.com>
2021-01-18 15:35:07 +0100

    creato tabella users

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 // includo User.php nel file index.php
 require('User.php');
 
-// creare alcune istanze della classe User per stampare i dati degli utenti
+// creare alcune istanze della classe User
 $user1 = new User;
 $user2 = new User;
 $user3 = new User;
@@ -10,4 +10,7 @@ $user3 = new User;
 var_dump($user1);
 var_dump($user2);
 var_dump($user3);
+
+// stampare i dati degli utenti
+
 ?>
